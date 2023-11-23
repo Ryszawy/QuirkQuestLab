1470f894b614c766396e90f6d620c6108de4f18b
Ryszawy <Krafi604@gmail.com>
2023-11-23 18:23:23 +0100

    add and remove product from favorites

--- src/test/groovy/UI/ProductTestSpec.groovy
+++ src/test/groovy/UI/ProductTestSpec.groovy
@@ -11,22 +11,39 @@ class ProductTestSpec extends UiUtils{
         getHomePageAsLoggedUser()
     }
 
-    def 'should add random product to favourite' () {
+    def 'should add random product to favorites' () {
         given:
-            def randomProductOnFirstPage = By.cssSelector("body > app-root > div > app-overview > div:nth-child(3) > div.col-md-9 > div.container > a:nth-child(1)")
+            def productName = getProductFromHomePage()
             def addToFavouriteBtn = By.xpath("//*[@id=\"btn-add-to-favorites\"]")
             def favouriteURL = "https://practicesoftwaretesting.com/#/account/favorites"
-        when:
-            def randomElementName = driver.findElement(randomProductOnFirstPage)
-            randomElementName.click()
-        then:
-            new WebDriverWait(driver, 3).until(ExpectedConditions.urlContains("https://practicesoftwaretesting.com/#/product"))
-        when:
-            def productName = driver.findElement(By.xpath("/html/body/app-root/div/app-detail/div[1]/div[2]/h1")).getText()
+        when: "add product to favorites"
             driver.findElement(addToFavouriteBtn).click()
-        then:
-            new WebDriverWait(driver, 3).until(ExpectedConditions.urlContains("https://practicesoftwaretesting.com/#/product"))
+            new WebDriverWait(driver, TIMEOUT).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='toast-body']")))
+            driver.get(favouriteURL)
+            new WebDriverWait(driver, TIMEOUT).until(ExpectedConditions.urlContains(favouriteURL))
+        then: "product is located in favorites list on user profile"
+            def products =  driver.findElements(By.xpath("//div[@data-test]"))
+            products.stream().anyMatch{it.getText().contains(productName)}
 
+    }
 
+    def 'should remove product from favorites' () {
+        given:
+            def productName = getProductFromHomePage()
+            def addToFavouriteBtn = By.xpath("//*[@id=\"btn-add-to-favorites\"]")
+            def favouriteURL = "https://practicesoftwaretesting.com/#/account/favorites"
+        when: "add product to favorites"
+            driver.findElement(addToFavouriteBtn).click()
+            new WebDriverWait(driver, TIMEOUT).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='toast-body']")))
+            driver.get(favouriteURL)
+            new WebDriverWait(driver, TIMEOUT).until(ExpectedConditions.urlContains(favouriteURL))
+        then: "locate added product in favorites list on user profile"
+            def products =  driver.findElements(By.xpath("//div[@data-test]"))
+            products.stream().anyMatch{it.getText().contains(productName)}
+            def favouriteProductElement = products.stream().filter{it.getText().contains(productName)}.findFirst()
+        when: "remove added product from favorites list"
+            favouriteProductElement.get().findElement(By.cssSelector("button.btn-danger.btn.mb-3")).click()
+        then: "product is delated"
+            new WebDriverWait(driver, TIMEOUT).until(ExpectedConditions.invisibilityOf(favouriteProductElement.get()))
     }
 }
